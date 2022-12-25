9bb3476a912a6ff1c7ec6867727ff2f134a87b9e
Duisenbek Ernur <eroxa2580@mail.ru>
2022-12-25 15:04:10 +0600

    cv

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,26 @@
 import "./style.css";
 
 const Header = () => {
-    return (
-		<header className="header">
-			<div className="header__wrapper">
-				<h1 className="header__title">
-					<strong>
-						Hi, my name is <em>Ernur</em>
-					</strong>
-					<br />a frontend developer
-				</h1>
-				<div className="header__text">
-					<p>with passion for learning and creating.</p>
-				</div>
-				<a href="#!" className="btn">
-					Download CV
-				</a>
-			</div>
-		</header>
-	);
-}
+  const pathToCv = "/cv.pdf";
+
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <h1 className="header__title">
+          <strong>
+            Hi, my name is <em>Ernur</em>
+          </strong>
+          <br />a frontend developer
+        </h1>
+        <div className="header__text">
+          <p>with passion for learning and creating.</p>
+        </div>
+        <a href={pathToCv} download className="btn" target="_blank">
+          Download CV
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
